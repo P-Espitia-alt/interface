f8e681549f671e0f7664d893b1247c9fe8aa7676
PaulaE <soypaulaespitia@gmail.com>
2026-03-05 17:51:25 -0500

    Interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Figura circulo = new Circulo(5);
+        Figura rectangulo = new Rectangulo(4, 6);
 
+        System.out.println("Área del círculo: " + circulo.calcularArea());
+        System.out.println("Perímetro del círculo: " + circulo.calcularPerimetro());
+
+        System.out.println("Área del rectángulo: " + rectangulo.calcularArea());
+        System.out.println("Perímetro del rectángulo: " + rectangulo.calcularPerimetro());
     }
 }
